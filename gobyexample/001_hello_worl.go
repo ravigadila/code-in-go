7749600c8240ff1b49f9910a57734bd65470fbea
ravigadila <ravikumar.gadila@gmail.com>
2023-08-20 15:09:23 +0530

    first progream

---
+++ gobyexample/001_hello_worl.go
@@ -0,0 +1,11 @@
+// package main indicates that this file should be compiled into exicutable comand
+// when you add below line you must add main function
+package main
+
+// fmt is basic go package
+import "fmt"
+
+// when you defin main(). go thinks this the starting poing.
+func main() {
+	fmt.Println("lets start learning go")
+}
